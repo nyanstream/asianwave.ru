737bcdb24a0096fb5063d27844178d15a0d8b761
Cojam <e965@protonmail.com>
2018-01-08 16:15:38 +0500

    2.3.5-dev 2

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 let
 	project =      require('./package.json'),
 	gulp =         require('gulp'),
+	tube =         require('gulp-pipe'),
 	bom =          require('gulp-bom'),
 	rename =       require('gulp-rename'),
 	watch =        require('gulp-watch'),
@@ -23,19 +24,27 @@ let
 	minifyJS = composer(uglifyjs, console),
 	reloadServer = () => live_server.stream()
 
+let folders = {
+	dev: 'source',
+	prod: {
+		root: 'build',
+		main: 'files'
+	}
+}
+
 let paths = {
 	html: {
-		dev: ['source/pug/**/*.pug', '!source/pug/inc/**/*.pug'],
-		prod: 'build/'
+		dev: [`${folders.dev}/pug/**/*.pug`, `!${folders.dev}/pug/inc/**/*.pug`],
+		prod: `${folders.prod.root}/`
 	},
 	js: {
-		dev: 'source/js/**/*.js',
-		prod: 'build/files/js/',
-		kamina: 'node_modules/kamina-js/dist/*.min.js',
+		dev: `${folders.dev}/js/**/*.js`,
+		prod: `${folders.prod.root}/${folders.prod.main}/js/`,
+		kamina: 'node_modules/kamina-js/dist/kamina.min.js',
 	},
 	css: {
-		dev: 'source/scss/**/*.scss',
-		prod: 'build/files/css/'
+		dev: `${folders.dev}/scss/**/*.scss`,
+		prod: `${folders.prod.root}/${folders.prod.main}/css/`
 	}
 }
 
@@ -45,11 +54,20 @@ gulp.task('liveReload', () => live_server({
 	notify: false
 }))
 
-gulp.task('pug', () => gulp.src(paths.html.dev)
-	.pipe(plumber())
-	.pipe(watch(paths.html.dev))
-	.pipe(pug({ locals: { VERSION: project.version } }))
-	.pipe(rename(file => {
+gulp.task('pug', () => tube([
+	gulp.src(paths.html.dev),
+	plumber(),
+	watch(paths.html.dev),
+	pug({ locals: {
+		VERSION: project.version,
+		PATHS: {
+			js: `/${folders.prod.main}/js`,
+			css: `/${folders.prod.main}/css`,
+			img: `/${folders.prod.main}/img`,
+			other: `/${folders.prod.main}/other`
+		}
+	}}),
+	rename(file => {
 		switch (file.dirname) {
 			case 'api':
 				file.extname = '.php'; break
@@ -57,38 +75,53 @@ gulp.task('pug', () => gulp.src(paths.html.dev)
 				file.dirname = `files/${file.dirname}`
 				file.extname = '.htm'
 		}
-	}))
-	.pipe(bom())
-	.pipe(gulp.dest(paths.html.prod))
-	.pipe(reloadServer())
-)
+	}),
+	bom(),
+	gulp.dest(paths.html.prod),
+	reloadServer()
+]))
 
-gulp.task('get-kamina', () => gulp.src(paths.js.kamina)
-	.pipe(bom())
-	.pipe(gulp.dest(paths.js.prod))
-)
+gulp.task('get-kamina', () => tube([
+	gulp.src(paths.js.kamina),
+	bom(),
+	gulp.dest(paths.js.prod)
+]))
 
-gulp.task('minify-js', () => gulp.src(paths.js.dev)
-	.pipe(plumber())
-	.pipe(watch(paths.js.dev))
-	.pipe(minifyJS({}))
-	.pipe(rename({suffix: '.min'}))
-	.pipe(bom())
-	.pipe(gulp.dest(paths.js.prod))
-	.pipe(reloadServer())
-)
+gulp.task('minify-js', () => tube([
+	gulp.src(paths.js.dev),
+	plumber(),
+	watch(paths.js.dev),
+	minifyJS({}),
+	rename({suffix: '.min'}),
+	bom(),
+	gulp.dest(paths.js.prod),
+	reloadServer()
+]))
 
-gulp.task('scss', () => sass.watch(paths.css.dev)
-	//gulp.src(paths.css.dev)
-	.pipe(plumber())
-	.pipe(sass.vars({ $VERSION: project.version }))
-	.pipe(sass.compile({outputStyle: 'compressed'}))
-	.pipe(csso())
-	.pipe(rename({suffix: '.min'}))
-	.pipe(bom())
-	.pipe(gulp.dest(paths.css.prod))
-	.pipe(reloadServer())
-)
+/*
+ * Раньше здесь была проблема, связанная с тем, что для SCSS нужен нормальный вочер, который понимает @import-ы, а единственный такой на npm работает не совсем правильно, и его нельзя использовать совместно с gulp.src. Поэтому приходится делать вот так.
+ * @TODO поискать более элегантное решение
+ */
+
+let scssTubes = [
+	plumber(),
+	sass.vars({ $VERSION: project.version }),
+	sass.compile({outputStyle: 'compressed'}),
+	csso(),
+	rename({suffix: '.min'}),
+	bom(),
+	gulp.dest(paths.css.prod)
+]
+
+gulp.task('scss:full', () => tube(
+	[gulp.src(paths.css.dev)].concat(scssTubes)
+))
+
+gulp.task('scss:dev', () => tube(
+	[sass.watch(paths.css.dev)].concat(scssTubes, [reloadServer()])
+))
+
+gulp.task('scss', () => gulp.parallel('scss:full', 'scss:dev'))
 
 gulp.task('default', gulp.parallel('pug', 'get-kamina', 'minify-js', 'scss'))
 gulp.task('dev', gulp.parallel('liveReload', 'default'))
